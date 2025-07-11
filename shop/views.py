06abb7797713ec05a766690b488d5804d280c949
SunilRajaan <beingsunilmagar@gmail.com>
2025-07-11 17:32:15 +0545

    Showing Multiple Product Slides On Homepage

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,10 @@ def index(request):
     print(products)
     n = len(products)
     nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range':range(nSlides), 'product':products}
+    # params = {'no_of_slides':nSlides, 'range':range(nSlides), 'product':products}
+    allProds = [[products, range(1, len(products)), nSlides],
+                [products, range(1, len(products)), nSlides]]
+    params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
 def about(request):
