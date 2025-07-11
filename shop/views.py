305a59a87636dfa4275f8e01c6ed997197c25bb6
SunilRajaan <beingsunilmagar@gmail.com>
2025-07-11 18:04:18 +0545

    Category-Wise Product Display in E-Commerce Website

--- shop/views.py
+++ shop/views.py
@@ -4,16 +4,26 @@ from .models import Product
 from math import ceil
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    # params = {'no_of_slides':nSlides, 'range':range(nSlides), 'product':products}
-    allProds = [[products, range(1, len(products)), nSlides],
-                [products, range(1, len(products)), nSlides]]
-    params = {'allProds':allProds}
+    allProds = []
+    catprods = Product.objects.values('category')  
+    cats = {item['category'] for item in catprods}
+
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = ceil(n / 4)
+        products_by_slide = [prod[i:i + 4] for i in range(0, n, 4)]
+        allProds.append([products_by_slide, cat, range(nSlides)])
+
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
+
 
 def about(request):
     return render(request, 'shop/about.html')
